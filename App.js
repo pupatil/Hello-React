64190a0876df8135c146f28c943dcfcc0ee5f0ef
pupatil <pooja760114@gmail.com>
2026-07-14 13:59:21 +0530

    Create element using javascript and React

--- App.js
+++ App.js
@@ -1,36 +1,53 @@
-/*
-<div i="Parent">
-    <div id="child">
-        <h1>I am H1 Tag</h1>
-        <h2>I am H2 Tag</h2>
-    </div>
- </div>
-*/
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const Parent = React.createElement("div" ,{id:"parent"}, 
-React.createElement("div" , {id:"child"}, 
-[
-React.createElement("h1" , {id:"heading1"} , "I am H1 tag"),
-React.createElement("h2" , {id:"heading1"} , "I am H2 tag")
-]),
-React.createElement("div" , {id:"child2"}, 
-[
-React.createElement("h3" , {id:"heading2"} , "I am H3 tag"),
-React.createElement("h4" , {id:"heading2"} , "I am H4 tag")
-]),
-)
+    // JXS traspiles before it reaches the browser. 
+    // It is not understood by the browser.
+    // So we need to use babel to transpile it into React.createElement() method which is understood by the browser.
+    // this is the way to create a react element using React.createElement() method
+
+    // JSX babel transpiles it into React.createElement() method which is understood by the browser.
+    // JSX => Babel tranpiles it to React.createElement() method => React.createElement object =>HTMLElement Render()
+    const heading = React.createElement("h1" , {},"Hello, React!");
+    const root= ReactDOM.createRoot(document.getElementById("root"))
 
 
+    // if we have single line jsx code then its fine to use it directly in the render method. But if we have multiple lines
+    //  of jsx code then we need to wrap it in a div or a angular bracket.
+    // this is the react Element because its start with small letter. 
+    const heading2 = (
+    <h1 className="heading" id="heading" tabIndex="1">
+        Hello, React! from JSX
+    </h1>
+    );
 
-// const heading1 = React.createElement(
-//     "h1", 
-//     {id:"heading", xyz:"ABC", className:"headingClass"},
-//     "Hello World from React*****");
+    // Wwe can create components in two ways. One is using function and another is using class.
+    //Functional Component:
+    // this is first way to create a functional component. We can also use arrow function to create a functional component.
+    // functinal compoen talwys start with the capital letter.
+    // use return syntax while writing the code
+    const TitleComponent = function(){
+        return(
+            <div>
+                <h1>Hello, React! from Title Functional Component</h1>
+            </div>
+        )
+    }
+    const HeadingComponent = () =>{
+       return( 
+       <div>
+        <TitleComponent />
+        <TitleComponent></TitleComponent>
+        {TitleComponent()}
+            <h1>Hello, React! from Functional Component</h1>
+        </div>
+        )
+    }
+    const HeadingComponents = () => <h1>Hello, React! from Functional Component , second way</h1>
+        
+    
 
-// console.log("Heading Means Object",heading1)
-const root1 = ReactDOM.createRoot(document.getElementById("root"));
-
-root1.render(Parent);
-
-
-
+// root.render(HeadingComponent); // we cant pass like this beacuse its a comopoen elament we can only pass in this way. we can also pass like this root.render(<HeadingComponent />); but we cant pass like this root.render(HeadingComponent()); because it will return a react element and we need to pass a react component.
+root.render(<HeadingComponent />); // we can also pass like this root.render(<HeadingComponent />); but we cant pass like this root.render(HeadingComponent()); because it will return a react element and we need to pass a react component.
+console.log(heading2);
+console.log(heading);
